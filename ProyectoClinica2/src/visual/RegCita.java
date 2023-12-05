1a974376342202417494200579089f532f39169a
Isaac G <Isaac G@laptop-05kbfb9c.taila126c.ts.net.>
2023-12-05 19:33:46 -0400

    Funciones para disponibilidad de doctores y bloqueo de boton

--- ProyectoClinica2/src/visual/RegCita.java
+++ ProyectoClinica2/src/visual/RegCita.java
@@ -63,9 +63,11 @@ public class RegCita extends JDialog {
 	private Persona miPersona = null;
 	private Doctor selected = null;
 	private static ArrayList<Cita> misCitas = Hospital.getInstance().getMisCitas();
+	private static ArrayList<Doctor> misDoctores = Hospital.getInstance().getMisDoctores();
 	private static ArrayList<Doctor> doctoresDisponibles = new ArrayList<>();
 	private static DefaultTableModel model;
 	private static Object[] row;
+	private JButton okButton;
 
 	/**
 	 * Launch the application.
@@ -82,44 +84,46 @@ public class RegCita extends JDialog {
 
 	/**
 	 * Create the dialog.
-	 * @throws ParseException 
+	 * 
+	 * @throws ParseException
 	 */
 	public RegCita() throws ParseException {
-		
+
 		setResizable(false);
 		setLocationRelativeTo(null); // Pantalla en el centro
-		setTitle("Cita No." + Hospital.getCodigoCita() );
+		setTitle("Cita No." + Hospital.getCodigoCita());
 		Calendar calenda = Calendar.getInstance();
 		Date fchActual = calenda.getTime();
-		
+
 		setBounds(100, 100, 564, 523);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		JPanel panel_Pasciente = new JPanel();
-		panel_Pasciente.setBorder(new TitledBorder(null, "Datos del Pasciente", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_Pasciente.setBorder(
+				new TitledBorder(null, "Datos del Pasciente", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_Pasciente.setBounds(10, 11, 280, 160);
 		contentPanel.add(panel_Pasciente);
 		panel_Pasciente.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("C\u00E9dula: ");
 		lblNewLabel.setBounds(10, 23, 75, 14);
 		panel_Pasciente.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Nombre: ");
 		lblNewLabel_1.setBounds(10, 48, 75, 14);
 		panel_Pasciente.add(lblNewLabel_1);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Telefono: ");
 		lblNewLabel_2.setBounds(10, 73, 62, 14);
 		panel_Pasciente.add(lblNewLabel_2);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Direcci\u00F3n: ");
 		lblNewLabel_3.setBounds(10, 98, 62, 14);
 		panel_Pasciente.add(lblNewLabel_3);
-		
+
 		rbtnMasculino = new JRadioButton("M");
 		rbtnMasculino.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -130,7 +134,7 @@ public class RegCita extends JDialog {
 		rbtnMasculino.setSelected(true);
 		rbtnMasculino.setBounds(110, 119, 47, 23);
 		panel_Pasciente.add(rbtnMasculino);
-		
+
 		rbtnFemenino = new JRadioButton("F");
 		rbtnFemenino.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -140,17 +144,29 @@ public class RegCita extends JDialog {
 		});
 		rbtnFemenino.setBounds(171, 119, 47, 23);
 		panel_Pasciente.add(rbtnFemenino);
-		
+
 		txtNombre = new JTextField();
+		txtNombre.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				habilitarBoton();
+			}
+		});
 		txtNombre.setColumns(10);
 		txtNombre.setBounds(68, 48, 187, 20);
 		panel_Pasciente.add(txtNombre);
-		
+
 		txtDireccion = new JTextField();
+		txtDireccion.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				habilitarBoton();
+			}
+		});
 		txtDireccion.setColumns(10);
 		txtDireccion.setBounds(68, 98, 187, 20);
 		panel_Pasciente.add(txtDireccion);
-		
+
 		JLabel lblNewLabel_4 = new JLabel("Sexo: ");
 		lblNewLabel_4.setBounds(10, 123, 47, 14);
 		panel_Pasciente.add(lblNewLabel_4);
@@ -160,97 +176,107 @@ public class RegCita extends JDialog {
 			txtCedula.addKeyListener(new KeyAdapter() {
 				@Override
 				public void keyReleased(KeyEvent e) {
-					if(txtCedula.getText().length() == 13) {
+
+					if (txtCedula.getText().length() == 13) {
 						miPersona = Hospital.getInstance().buscarPersonaByCedula(txtCedula.getText());
-						if(miPersona!=null)
-						{
+						if (miPersona != null) {
 							cargarPersona(miPersona);
 						}
-					}else {
+					} else {
 						borrarCampos();
 					}
-					
+					habilitarBoton();
 				}
 			});
 			txtCedula.setBounds(68, 20, 187, 20);
 			panel_Pasciente.add(txtCedula);
-			
+
 			MaskFormatter formatterTelefono = new MaskFormatter("###-###-####");
 			txtTelefono = new JFormattedTextField(formatterTelefono);
+			txtTelefono.addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyReleased(KeyEvent e) {
+					habilitarBoton();
+				}
+			});
 			txtTelefono.setBounds(68, 73, 187, 20);
 			panel_Pasciente.add(txtTelefono);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		JPanel panel_Fecha = new JPanel();
-		panel_Fecha.setBorder(new TitledBorder(null, "Fecha de la Cita", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_Fecha.setBorder(
+				new TitledBorder(null, "Fecha de la Cita", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_Fecha.setBounds(10, 182, 535, 258);
 		contentPanel.add(panel_Fecha);
 		panel_Fecha.setLayout(null);
-		
+
 		calendario = new JCalendar();
 		calendario.getDayChooser().setWeekOfYearVisible(false);
 		calendario.addPropertyChangeListener(new PropertyChangeListener() {
 			public void propertyChange(PropertyChangeEvent evt) {
-				cargarDoctoresDisponibles();
-				
+				cargarDoctoresDisponibles(calendario.getDate(), (Date) spnHoraInicio.getValue(),
+						(Date) spnHoraFin.getValue());
 			}
 		});
 		calendario.setBounds(10, 28, 366, 219);
 		panel_Fecha.add(calendario);
-		
+
 		JPanel panel_Inicio = new JPanel();
-		panel_Inicio.setBorder(new TitledBorder(null, "Hora de Inicio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_Inicio.setBorder(
+				new TitledBorder(null, "Hora de Inicio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_Inicio.setBounds(386, 23, 139, 93);
 		panel_Fecha.add(panel_Inicio);
 		panel_Inicio.setLayout(null);
-		
+
 		spnHoraInicio = new JSpinner();
 		spnHoraInicio.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
-				cargarDoctoresDisponibles();
-				
+				cargarDoctoresDisponibles(calendario.getDate(), (Date) spnHoraInicio.getValue(),
+						(Date) spnHoraFin.getValue());
 			}
 		});
 		spnHoraInicio.setModel(new SpinnerDateModel(fchActual, null, null, Calendar.HOUR_OF_DAY));
 		JSpinner.DateEditor de_spnHoraInicio = new JSpinner.DateEditor(spnHoraInicio, "h:mm a");
 		spnHoraInicio.setEditor(de_spnHoraInicio);
-		
+
 		spnHoraInicio.setBounds(10, 37, 119, 20);
 		panel_Inicio.add(spnHoraInicio);
-		
+
 		JPanel panel_Fin = new JPanel();
-		panel_Fin.setBorder(new TitledBorder(null, "Hora de Finalizaci\u00F3n", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_Fin.setBorder(new TitledBorder(null, "Hora de Finalizaci\u00F3n", TitledBorder.LEADING, TitledBorder.TOP,
+				null, null));
 		panel_Fin.setBounds(386, 140, 139, 93);
 		panel_Fecha.add(panel_Fin);
 		panel_Fin.setLayout(null);
-		
+
 		spnHoraFin = new JSpinner();
 		spnHoraFin.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
-				cargarDoctoresDisponibles();
-				
+				cargarDoctoresDisponibles(calendario.getDate(), (Date) spnHoraInicio.getValue(),
+						(Date) spnHoraFin.getValue());
 			}
 		});
 		spnHoraFin.setModel(new SpinnerDateModel(fchActual, null, null, Calendar.HOUR_OF_DAY));
 		JSpinner.DateEditor de_spnHoraFin = new JSpinner.DateEditor(spnHoraFin, "h:mm a");
 		spnHoraFin.setEditor(de_spnHoraFin);
-		
+
 		spnHoraFin.setBounds(10, 42, 119, 20);
 		panel_Fin.add(spnHoraFin);
-		
+
 		JPanel panel_Doctor = new JPanel();
-		panel_Doctor.setBorder(new TitledBorder(null, "Doctores Disponibles", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_Doctor.setBorder(
+				new TitledBorder(null, "Doctores Disponibles", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_Doctor.setBounds(300, 11, 245, 160);
 		contentPanel.add(panel_Doctor);
 		panel_Doctor.setLayout(null);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 23, 225, 126);
 		panel_Doctor.add(scrollPane);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
+
 		String[] header = { "Id", "Nombre" };
 		model = new DefaultTableModel();
 		model.setColumnIdentifiers(header);
@@ -259,7 +285,7 @@ public class RegCita extends JDialog {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				int index = tblDoctores.getSelectedRow();
-				if(index>=0) {
+				if (index >= 0) {
 					selected = Hospital.getInstance().buscarDoctorById(tblDoctores.getValueAt(index, 0).toString());
 				}
 			}
@@ -271,15 +297,15 @@ public class RegCita extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
+				okButton = new JButton("OK");
 				okButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
-						if(miPersona == null)
-						{
+						if (miPersona == null) {
 							String NHC = formatearNumero(String.valueOf(Hospital.getCodigoPaciente()));
-							miPersona = new Paciente(NHC, txtCedula.getText(),txtNombre.getText(),txtTelefono.getText(), txtDireccion.getText(), determimarSexo());
+							miPersona = new Paciente(NHC, txtCedula.getText(), txtNombre.getText(),
+									txtTelefono.getText(), txtDireccion.getText(), determimarSexo());
 						}
-						Date fchProgramada = determinarFecha(calendario.getDate() , (Date) spnHoraFin.getValue());
+						Date fchProgramada = determinarFecha(calendario.getDate(), (Date) spnHoraFin.getValue());
 						Cita nuevaCita = new Cita("C-" + Hospital.getCodigoCita(), miPersona, selected, fchProgramada);
 						Hospital.getInstance().addCita(nuevaCita);
 					}
@@ -302,30 +328,40 @@ public class RegCita extends JDialog {
 		}
 		imprimirDoctores(doctoresDisponibles);
 	}
+
+	private void habilitarBoton() {
+		if (!txtCedula.getText().isEmpty() && !txtNombre.getText().isEmpty() && !txtDireccion.getText().isEmpty()
+				&& !txtTelefono.getText().isEmpty() && selected != null) {
+			okButton.setEnabled(true);
+		} else {
+			okButton.setEnabled(false);
+		}
+	}
+
 	private static Date determinarFecha(Date fecha, Date hora) {
-        if (fecha == null || hora == null) {
-            return null;
-        }
-        Calendar calFecha = Calendar.getInstance();
-        calFecha.setTime(fecha);
+		if (fecha == null || hora == null) {
+			return null;
+		}
+		Calendar calFecha = Calendar.getInstance();
+		calFecha.setTime(fecha);
 
-        Calendar calHora = Calendar.getInstance();
-        calHora.setTime(hora);
+		Calendar calHora = Calendar.getInstance();
+		calHora.setTime(hora);
 
-        // Configurar la hora de la fecha
-        calFecha.set(Calendar.HOUR_OF_DAY, calHora.get(Calendar.HOUR_OF_DAY));
-        calFecha.set(Calendar.MINUTE, calHora.get(Calendar.MINUTE));
-        calFecha.set(Calendar.SECOND, calHora.get(Calendar.SECOND));
-        calFecha.set(Calendar.MILLISECOND, calHora.get(Calendar.MILLISECOND));
-        return calFecha.getTime();
-    }
+		// Configurar la hora de la fecha
+		calFecha.set(Calendar.HOUR_OF_DAY, calHora.get(Calendar.HOUR_OF_DAY));
+		calFecha.set(Calendar.MINUTE, calHora.get(Calendar.MINUTE));
+		calFecha.set(Calendar.SECOND, calHora.get(Calendar.SECOND));
+		calFecha.set(Calendar.MILLISECOND, calHora.get(Calendar.MILLISECOND));
+		return calFecha.getTime();
+	}
 
-	private void cargarPersona(Persona aux ){
+	private void cargarPersona(Persona aux) {
 		txtNombre.setText(aux.getNombre());
 		txtTelefono.setText(aux.getTelefono());
 		txtDireccion.setText(aux.getDireccion());
-		
-		if(aux.esMasculino()) {
+
+		if (aux.esMasculino()) {
 			rbtnMasculino.setSelected(true);
 			rbtnMasculino.setSelected(false);
 			return;
@@ -333,53 +369,64 @@ public class RegCita extends JDialog {
 		rbtnMasculino.setSelected(false);
 		rbtnMasculino.setSelected(true);
 	}
-	private void borrarCampos()
-	{
+
+	private void borrarCampos() {
 		txtNombre.setText("");
 		txtTelefono.setText("");
 		txtDireccion.setText("");
 		rbtnMasculino.setSelected(true);
 		rbtnFemenino.setSelected(false);
 	}
-	private ArrayList<Doctor> cargarDoctoresDisponibles() {
+
+	private ArrayList<Doctor> cargarDoctoresDisponibles(Date fechaConsulta, Date horaInicio, Date horaFin) {
 		ArrayList<Doctor> doctoresDisponibles = new ArrayList<>();
-		Calendar calFecha = Calendar.getInstance();
-		Calendar calHoraInicio = Calendar.getInstance();
-		Calendar calHoraFin = Calendar.getInstance();
-        calFecha.setTime(calendario.getDate());
-        calHoraInicio.setTime((Date) spnHoraInicio.getValue());
-        calHoraFin.setTime((Date) spnHoraFin.getValue());
-        
-        for (Cita cita : misCitas) {
-			
+		/*
+		 * Calendar calFecha = Calendar.getInstance(); Calendar calHoraInicio =
+		 * Calendar.getInstance(); Calendar calHoraFin = Calendar.getInstance();
+		 * calFecha.setTime(calendario.getDate()); calHoraInicio.setTime((Date)
+		 * spnHoraInicio.getValue()); calHoraFin.setTime((Date) spnHoraFin.getValue());
+		 */
+		for (Doctor miDoctor : misDoctores) {
+			boolean ocupado = false;
+			for (Cita miCita : misCitas) {
+				if (miCita.getMiDoctor().equals(miDoctor) && miCita.getFchProgramada().equals(fechaConsulta)
+						&& (miCita.getFchProgramada().after(horaInicio) || miCita.getFchProgramada().equals(horaInicio))
+						&& (miCita.getFchProgramada().before(horaFin) || miCita.getFchProgramada().equals(horaFin))) {
+					ocupado = true;
+					break;
+				}
+			}
+			if (!ocupado) {
+				doctoresDisponibles.add(miDoctor);
+			}
 		}
-			
+		selected = null;
 		return doctoresDisponibles;
 	}
-	private static void imprimirDoctores(ArrayList<Doctor> losDoctores)
-	{
+
+	private static void imprimirDoctores(ArrayList<Doctor> losDoctores) {
 		model.setRowCount(0);
-		row =new Object[model.getColumnCount()];
-		for (Doctor aux: losDoctores)
-		{
+		row = new Object[model.getColumnCount()];
+		for (Doctor aux : losDoctores) {
 			row[0] = aux.getId();
 			row[1] = aux.getNombre();
 			model.addRow(row);
 		}
 	}
-    private String formatearNumero(String numero) {
-        while (numero.length() < 6) {
-            numero = "0" + numero;
-        }
-        // Formatear "###-##-##"
-        return numero.substring(0, 3) + "-" + numero.substring(3, 5) + "-" + numero.substring(5, 7);
-    }
-    private String determimarSexo() {
-		if(rbtnFemenino.isSelected())
-		{
+
+	private String formatearNumero(String numero) {
+		while (numero.length() < 6) {
+			numero = "0" + numero;
+		}
+		// Formatear "###-##-##"
+		return numero.substring(0, 3) + "-" + numero.substring(3, 5) + "-" + numero.substring(5, 7);
+	}
+
+	private String determimarSexo() {
+		if (rbtnFemenino.isSelected()) {
 			return "Femenino";
 		}
 		return "Masculino";
 	}
- 
+
 }

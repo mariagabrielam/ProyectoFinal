f7772355a31d784996fee416e3e9d443882d00b5
Owner <Owner@DESKTOP-R6JHS8I>
2023-12-06 13:54:19 -0400

    añadir usuario admin para la primera vez

--- ProyectoClinica2/src/visual/Login.java
+++ ProyectoClinica2/src/visual/Login.java
@@ -23,6 +23,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
 import logico.Hospital;
+import logico.Usuario;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -57,9 +59,10 @@ public class Login extends JDialog {
 					empresaRead.close();
 				} catch (FileNotFoundException e) {
 					try {
-						empresa2 = new  FileOutputStream("empresa.dat");
+						empresa2 = new  FileOutputStream("hospital.dat");
 						empresaWrite = new ObjectOutputStream(empresa2);
 						empresaWrite.writeObject(Hospital.getInstance());
+						Usuario admin = new Usuario("Admin", "Admin", null, "Admin");
 						empresa2.close();
 						empresaWrite.close();
 					} catch (FileNotFoundException e1) {
@@ -88,13 +91,13 @@ public class Login extends JDialog {
 	 * Create the dialog.
 	 */
 	public Login() {
-		if(Hospital.getInstance().getMisUsuarios()==null)
+		/*if(Hospital.getInstance().getMisUsuarios()==null)
 		{
 			JOptionPane.showMessageDialog(null, "Aun no hay usuarios creados. Cree el usuario de administración", "Falta de Usuario", JOptionPane.ERROR_MESSAGE);
 			CrearUsuario crearUsuario = new CrearUsuario(false);
 			crearUsuario.setModal(true);
 			crearUsuario.setVisible(true);
-		}
+		}*/
 		setTitle("Login");
 		setBounds(100, 100, 410, 261);
 		getContentPane().setLayout(new BorderLayout());
